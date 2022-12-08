96a7475b5732a34d568dc4060c7f366589fbbb6c
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-08 18:34:40 +0000

    Note Form Functionality

--- notes-project/src/App.tsx
+++ notes-project/src/App.tsx
@@ -1,12 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
+import NewNote from "./NewNote";
+export type Note = {
+  id: string;
+} & NotesData;
+
+export type NotesData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
 function App() {
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<h1>h1</h1>} />
-        <Route path="/new" element={<h1>new</h1>} />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
         <Route path="*" element={<Navigate to="/" />} />
 
         <Route path="/:id">
